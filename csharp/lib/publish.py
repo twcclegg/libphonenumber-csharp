535e93eb6073d779b54470416ff6dd331ad490eb
Patrick Mezard <patrick@mezard.eu>
2012-04-28 10:50:10 +0200

    publish: fix cleanup step

--- csharp/lib/publish.py
+++ csharp/lib/publish.py
@@ -19,7 +19,7 @@ def haschanges(ui):
     return stdout.strip().endswith('+')
 
 def purge(ui):
-    subprocess.check_call(['hg', '-R', ui.rootdir, 'purge', '--all',
+    subprocess.check_call(['hg', '--cwd', ui.rootdir, 'purge', '--all',
                            '-X', 'csharp/PhoneNumbers.Test'],
                           shell=True)
 
