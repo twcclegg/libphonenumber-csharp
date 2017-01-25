5b8b1902d4afe6d2fe4ca4f5f241240322c0901c
Caelan Sayler <Caelan.Sayler@bluestone.co.uk>
2017-01-25 17:32:33 +0000

    Update c-sharp metadata to v8.1.0

--- csharp/PhoneNumbers/Properties/AssemblyInfo.cs
+++ csharp/PhoneNumbers/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("4.10.0.62")]
-[assembly: AssemblyVersion("7.7.5")]
-[assembly: AssemblyFileVersion("7.7.5")]
+[assembly: AssemblyVersion("8.1.0")]
+[assembly: AssemblyFileVersion("8.1.0")]
