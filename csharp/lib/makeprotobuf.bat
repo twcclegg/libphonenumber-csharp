40f5ffddff918f7231da99c9099373c45a180dc4
Tom Clegg <tom@clegg.xyz>
2018-05-31 14:52:26 -0700

    cleaning house

--- csharp/lib/makeprotobuf.bat
+++
@@ -1,9 +0,0 @@
-REM This script is used to generate Phonemetadata.cs and Phonenumber.cs
-REM from their protocol buffers definitions.
-REM Pythonscript depends on Python v2.7
-
-protogen -namespace=PhoneNumbers -cls_compliance=false --proto_path=..\..\resources ..\..\resources\phonemetadata.proto ..\..\resources\phonenumber.proto
-python cleanprotobuf.py PhoneMetadata.cs ..\PhoneNumbers\PhoneMetadata.cs
-python cleanprotobuf.py PhoneNumber.cs ..\PhoneNumbers\PhoneNumber.cs
-del PhoneMetadata.cs
-del PhoneNumber.cs
