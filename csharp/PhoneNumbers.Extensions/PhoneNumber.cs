f6731a6314f660d64aec79c8241eea52e9b2f4a8
Tom Clegg <t@clegg.dev>
2022-11-28 15:06:43 -0600

    add Extensions project

---
+++ csharp/PhoneNumbers.Extensions/PhoneNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PhoneNumbers.Extensions
+{
+    public static class PhoneNumber
+    {
+        private static readonly PhoneNumberUtil PhoneNumberUtil = PhoneNumberUtil.GetInstance();
+
+        public static bool TryParse(string input, string region, out PhoneNumbers.PhoneNumber number)
+        {
+            try
+            {
+                number = PhoneNumberUtil.Parse(input, region);
+                return true;
+            }
+            catch
+            {
+                number = null;
+                return false;
+            }
+        }
+
+        public static bool TryParseValid(string input, string region, out PhoneNumbers.PhoneNumber number)
+        {
+            try
+            {
+                number = PhoneNumberUtil.Parse(input, region);
+                if (PhoneNumberUtil.IsValidNumber(number))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+
+            number = null;
+            return false;
+        }
+    }
+}
