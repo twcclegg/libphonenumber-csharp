d688fb9f16fbc093afad1071b34703175baaff2b
Wilson Mun <30316250+wmundev@users.noreply.github.com>
2024-03-31 20:36:32 +1100

    feat: add performance tests for format method

---
+++ csharp/PhoneNumbers.PerformanceTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace PhoneNumbers.PerformanceTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<PhoneNumberFormatBenchmark>();
+        }
+    }
+}
