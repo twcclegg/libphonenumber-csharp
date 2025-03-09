d7124e6b606902e8a2ae69f9db0ab6a03c560f7d
Thomas Clegg <t@clegg.dev>
2025-03-08 19:06:45 -0600

    #if

--- csharp/PhoneNumbers/Util.cs
+++ csharp/PhoneNumbers/Util.cs
@@ -19,8 +19,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
+#if !SIGNED
 [assembly: InternalsVisibleTo("PhoneNumbers.Test")]
+#else
 [assembly: InternalsVisibleTo("PhoneNumbers.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100bd3070027f51a9975cac34376755e3629985626c0ccbb41bb057f7d06dd6940dafb35ed0358fd96f24525cde3229cecc6fc9eb3bf582ecb6cf3a837f422d38fe2f5d2d7d0b75a5fe9120c77d3a0d25b9b60060cd715146920d675b6f639bcf9845bcf0f42070caca24be55143958dcc4eaa7e4e2941ecf2fab4ba479aaee8dc2")]
+#endif
 
 namespace PhoneNumbers
 {
